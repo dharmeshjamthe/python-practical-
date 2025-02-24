036cd5f27cbf1c9a942bf607d5fba26a1749555f
dharmeshjamthe <dharmeshjamthe@gmail.com>
2025-02-25 00:39:16 +0530

    code push

---
+++ sumofallnumbersinlist.py
@@ -0,0 +1,14 @@
+# List of numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Using a for loop
+total_sum_loop = 0
+for number in numbers:
+    total_sum_loop += number
+
+print("Sum of all numbers using for loop:", total_sum_loop)
+
+# Using the built-in sum() function
+total_sum_builtin = sum(numbers)
+
+print("Sum of all numbers using sum() function:", total_sum_builtin)
