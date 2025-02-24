d82a3cbc80982ed6d7ae3636f811c0b0ff4f8594
dharmeshjamthe <dharmeshjamthe@gmail.com>
2025-02-24 09:34:24 +0530

    modified

--- countuntil0.py
+++ countuntil0.py
@@ -2,6 +2,6 @@ while True:
     number = int(input("Enter a number (0 to exit): "))
     if number == 0:
         break
-    print("you entered:",number)
+    print("you entered:", number)
 
 print("Exited the loop.")
