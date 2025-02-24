8e1805bbcd6971a1a74ed2921f227db88a5fa5f5
dharmeshjamthe <dharmeshjamthe@gmail.com>
2025-02-24 08:52:33 +0530

    code push

---
+++ countuntil0.py
@@ -0,0 +1,7 @@
+while True:
+    number = int(input("Enter a number (0 to exit): "))
+    if number == 0:
+        break
+    print("you entered:",number)
+
+print("Exited the loop.")
--- oddeven.py
+++ oddeven.py
@@ -1,6 +1,5 @@
-
 number = int(input("Enter a number: "))
 if number % 2 == 0:
-    print(f"{number} is even.")
+    print(number ,"is even.")
 else:
-    print(f"{number} is odd.").
+    print(number ,"is odd.")
